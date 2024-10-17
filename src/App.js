ace95b06d1d7bf4ac755e90478114d96856d42ef
niranjankatkar <niranjankakatkar.999@gmail.com>
2024-10-17 16:47:19 +0530

    17-10-2024

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './login';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
     <Routes>
       <Route path="/" element={<Login />}></Route>
+      <Route path="/home" element={<Home />}></Route>
    
     </Routes>
   </Router>
